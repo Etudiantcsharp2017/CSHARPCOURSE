136b83cf12d438d273bf88683ab0f1c62d4d5504
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2016-12-13 11:12:16 +0000

    VARIBLE BOOLEAN Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

--- DataTypes_Char_StringBoolTypes/DataTypes_Char_StringBoolTypes/Program.cs
+++ DataTypes_Char_StringBoolTypes/DataTypes_Char_StringBoolTypes/Program.cs
@@ -16,6 +16,12 @@ namespace DataTypes_Char_StringBoolTypes
             char mychar = 'G';
             int convchtoint = Convert.ToInt32('G');
             Console.WriteLine("Min char ={0} Max char ={1} My Charachter ={2} My Conversion ={3}",charMin,charMax,mychar,convchtoint);
+            string MyString = "This is my Varible";
+            Console.WriteLine("\nMy String is {0}", MyString);
+
+            bool MyBool = false;
+            Console.WriteLine("My Boolean Value is {0}", MyBool);
+
             Console.ReadKey();
         }
     }
