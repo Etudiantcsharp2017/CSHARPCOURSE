024acc15ca92dc0f615b69285cebe75e0064dc84
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2016-12-13 11:05:43 +0000

    Conversion char to Int Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

--- DataTypes_Char_StringBoolTypes/DataTypes_Char_StringBoolTypes/Program.cs
+++ DataTypes_Char_StringBoolTypes/DataTypes_Char_StringBoolTypes/Program.cs
@@ -14,8 +14,8 @@ namespace DataTypes_Char_StringBoolTypes
             int charMin = char.MinValue;
 
             char mychar = 'G';
-
-            Console.WriteLine("Min char ={0} Max char ={1} My Charachter ={2}",charMin,charMax,mychar);
+            int convchtoint = Convert.ToInt32('G');
+            Console.WriteLine("Min char ={0} Max char ={1} My Charachter ={2} My Conversion ={3}",charMin,charMax,mychar,convchtoint);
             Console.ReadKey();
         }
     }
