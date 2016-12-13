70dc4f8f46729cbe2b020ede59af04fd01b4eec5
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2016-12-13 09:26:06 +0000

    First commit

---
+++ MyVaribles/MyVaribles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVaribles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MyValue = 7;
+            Console.WriteLine("{0}", MyValue);
+            Console.ReadKey();
+        }
+    }
+}
