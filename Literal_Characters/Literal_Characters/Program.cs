68d6560257f51eb1d684f12ea232b61a7514a52e
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2016-12-13 15:18:12 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ Literal_Characters/Literal_Characters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = -100;
+            uint myUvar = 1000U;
+            ulong myLvar = 1000uL;
+            float myFlvar = 1.5F;
+            double myDvar = 100.5D;
+            decimal myDecvar = 100.5M;
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", myVar,myUvar,myLvar,myFlvar,myDvar,myDecvar);
+            Console.WriteLine($"{myDecvar},{myDvar}");
+            Console.ReadKey();
+        }
+    }
+}
