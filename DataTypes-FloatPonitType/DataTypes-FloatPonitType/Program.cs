9bdcc68981233188420ee74e2059a979d951bed5
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2016-12-13 10:50:27 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ DataTypes-FloatPonitType/DataTypes-FloatPonitType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_FloatPonitType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+            Console.WriteLine("Min Value {0}, Max Value {1} ", flMin, flMax);
+            float flVar = 1F / 3;
+            Double dlVar = 1d / 3;
+            decimal dcVar = 1M / 3;
+            Console.WriteLine("Float Varible {0}\nDouble Varible {1}\nDecimal Varible {2}", flVar, dlVar, dcVar);
+            Console.ReadKey();
+        }
+    }
+}
