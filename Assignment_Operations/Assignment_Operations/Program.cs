de6e50ac6ffdcbcb7fe93302f9b321d736beae50
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2016-12-14 14:09:10 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

--- Assignment_Operations/Assignment_Operations/Program.cs
+++ Assignment_Operations/Assignment_Operations/Program.cs
@@ -10,7 +10,21 @@ namespace Assignment_Operations
     {
         static void Main(string[] args)
         {
-            //
+            //=, += ,-=,*=,/=,%=
+            //int myvar1 =7, myvar2 = 3;
+            //myvar2 = myvar1;
+            //myvar2 += myvar1; ///myvar1=myvar1+myvar2
+            //myvar1 -= myvar2;//myvar1=myvar1-myvar2
+            // myvar1 *= myvar2;//myvar1=myvar1*myvar2
+            // myvar1 /= myvar2;//myvar1=myvar1/myvar2
+            //myvar1 %= myvar2; //myvar1=myvar1%myvar2
+            //Console.WriteLine( "Th Result is={0} the value of Var2 ={1}",myvar1,myvar2);
+            ////////////////////////Q1
+            int temp, myvar1 = 7, myvar2 = 2;
+            myvar1 /= ++myvar2;
+            Console.WriteLine("The result is {0}", myvar1);
+            Console.ReadKey();
+
         }
     }
 }
