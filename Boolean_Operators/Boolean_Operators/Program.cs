2771e4b1bfb45cef84e380b4a3e46ffa26c4d4af
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2016-12-15 08:39:14 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ Boolean_Operators/Boolean_Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boolean Comparison ==,!=,<,>,<=,>=
+            //Boolean Condition &&,||
+            bool myresult;
+            int myvar1 = 5, myvar2 = 2;
+            myresult = myvar1 != 8;
+            myresult = (myvar1 == 5) || (myvar2 == 8);
+            Console.WriteLine("My result is = {0}",myresult);
+            int mvar1 = 9, mvar2 = 11;bool test;
+            test = (mvar1 < 10) && (mvar2 <= 11);
+            Console.WriteLine("The result is ={0}", test);
+            test= (mvar1 < 10) || (mvar2 <= 11);
+            Console.WriteLine("The result is ={0}", test);
+            Console.ReadKey();
+
+        }
+    }
+}
