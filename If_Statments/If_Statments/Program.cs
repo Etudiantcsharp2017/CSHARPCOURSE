a9df238deae26fe177e569afe273873d326ca00a
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2017-01-05 13:51:10 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ If_Statments/If_Statments/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVAR = 100;
+            if (myVAR == 10)
+                Console.WriteLine("My variable equel 10");
+            else
+                Console.WriteLine("My variable not equel 10");
+            Console.ReadKey();
+        }
+    }
+}
