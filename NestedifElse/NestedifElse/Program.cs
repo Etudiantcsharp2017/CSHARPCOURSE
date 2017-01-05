d33f8790b817def88dac60807bd653ac9298f2a4
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2017-01-05 14:36:07 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ NestedifElse/NestedifElse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 4, myVar2 = 6;
+            if(myVar<=3 && myVar>=5)
+            {
+                Console.WriteLine("Condition one rignt ");
+            }
+            else if (myVar<=4 && myVar2>=6)
+            {
+                Console.WriteLine("Condition two rignt");
+            }
+            else if (myVar2>5)
+            {
+                Console.WriteLine("Condition three rignt");
+            }
+            else
+            {
+                Console.WriteLine("No one of the conditions ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
