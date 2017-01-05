86f52ead02a3fd0c80d9b62a22d5fec227d734e1
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2017-01-05 14:20:53 +0000

    Net_if stetments

---
+++ Nested_If_Stetements/Nested_If_Stetements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_If_Stetements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 9, myVar2 = 6;
+            if(myVar1==10)
+            {
+                Console.WriteLine("myVar equal 9 ");
+                if (myVar2==7)
+                {
+                    Console.WriteLine("myVar ==7 ");
+                }
+                else
+                {
+                    Console.WriteLine("not equal myVar == 7");
+                }
+            }
+            else
+            {
+                Console.WriteLine("not equal myVar ==9 ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
