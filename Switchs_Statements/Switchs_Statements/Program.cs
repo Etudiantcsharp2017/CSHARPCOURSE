2f40e232b160549a2ce5c1f6441a7af5f4b7c53e
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2017-01-08 09:51:04 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ Switchs_Statements/Switchs_Statements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switchs_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myAnimal = "Cat";
+            //switch (myAnimal)
+            //{
+            //    case "Canary":
+            //        Console.WriteLine("my aanimal is Canary");
+            //        break;
+            //    case "Cat":
+            //        Console.WriteLine("my aanimal is Cat");
+            //        break;
+            //    case "Camel":
+            //        Console.WriteLine("my aanimal is Camel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("no one");
+            //        break;
+            //}
+            string whatHesay = "Hello";
+            switch (whatHesay)
+            {
+                case "hello":
+                    Console.WriteLine("hello");
+                    break;
+                case "welcom":
+                    Console.WriteLine("welcom");
+                    break;
+                case "Bey":
+                    Console.WriteLine("Bey");
+                    break;
+                default:
+                    Console.WriteLine("No One");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
