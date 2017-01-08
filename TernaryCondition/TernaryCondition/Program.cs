c8178d998acc898ecce6df1acdd42398390ee3b2
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2017-01-08 13:35:08 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ TernaryCondition/TernaryCondition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryCondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int myVar = 99;
+            //   string myResult = (myVar < 10) ? "Less than 10" : "Gereater than 10 ";
+            //// int myResult = (myVar < 10) ? 1 : 0;
+            // Console.WriteLine("My variable Result is {0}", myResult);
+            string myName = "kamel", result;
+            result = (myName.Equals("kamel2")) ? "Yes" : "No";
+            Console.WriteLine("My result is : {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
