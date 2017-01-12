fcfa76295a2fbdee7ba2fe847714f39bb34105e3
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2017-01-12 09:08:05 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ Break Loop _ Continue/Break Loop _ Continue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop___Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=5; i++)
+            {
+                Console.WriteLine("Hello Kamel");
+                if (i > 2)
+                
+                 
+                    continue;
+                   
+                
+                for (int j = 0; j <=4; j++)
+                {
+                    Console.WriteLine("\tHello word");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
