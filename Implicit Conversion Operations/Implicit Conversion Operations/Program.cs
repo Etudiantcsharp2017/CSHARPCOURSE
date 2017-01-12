a0865a326de47269cebef001c0b4372c3b4b033c
Etudiantcsharp2017 <boussayoudmail@gmail.com>
2017-01-12 14:15:34 +0000

    Signed-off-by: Etudiantcsharp2017 <boussayoudmail@gmail.com>

---
+++ Implicit Conversion Operations/Implicit Conversion Operations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_Conversion_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myVar = 1000;
+            float myVar2 = myVar;
+            float myfloatVvar = 12.5f;
+            int myInt2Var = myfloatVvar;                
+            Console.WriteLine($"{myVar2}");
+           // Console.WriteLine(myVar2);
+            char myVar3= 'G';
+            int myIntvar = myVar3;
+            Console.WriteLine($"{myIntvar}");
+            Console.ReadKey();
+        }
+    }
+}
